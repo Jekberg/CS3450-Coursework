605c59f6603bbe7ed7a0c3dec4912bd42d561b87
unknown <john.berg.96bits@gmail.com>
2020-03-18 22:04:25 +0000

    Improving the menu system

---
+++ Assets/Scripts/Menu/MenuSystem.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    public void Launch()
+    {
+        SceneManager.LoadSceneAsync("TestArea");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void NavigateTo(string name)
+    {
+        MenuManager.PushMenu(name);
+    }
+    
+    public void NavigateBack()
+    {
+        MenuManager.PopMenu();
+    }
+}
